4a7d430fd54c3464189a271ad8220ccec61065e1
Maxime Cliche <mcliche91@gmail.com>
2021-11-28 15:31:43 -0500

    Update gitignore to ignore .vscode folder

--- conf/config.ts
+++ conf/config.ts
@@ -6,7 +6,7 @@ export const Config = {
 		MAX_ASYNC_PAGE:		3,
 		LOOP_INTERVAL_MS:	500,
 		PUPPETEER_HEADLESS:	false,
-		SLOWDOWN_MS:		2000, // Added to slow down puppeteer to reduce the change of getting blocked
+		SLOWDOWN_MS:		2000,	// Added to slow down puppeteer to reduce the change of getting blocked
 		PROXY_URL:			"",		// Include protocol (http://, https://...)
 		PROXY_AUTH:			{
 			username: "",
