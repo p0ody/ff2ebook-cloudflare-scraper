bad1a38d1689b969080a697771e5800dc1c99dea
Maxime Cliche <mcliche91@gmail.com>
2021-11-29 18:34:48 -0500

    Update config file

--- conf/config.ts
+++ conf/config.ts
@@ -1,18 +1,19 @@
 export const Config = {
 	App: {
-		LISTENING_PORT: 	4000,
+		LISTENING_PORT:		3000,
 	},
 	ScraperMgr: {
-		MAX_ASYNC_PAGE:		3,
-		LOOP_INTERVAL_MS:	500,
+		MAX_ASYNC_PAGE:		4,
+		LOOP_INTERVAL_MS:	700,
 		PUPPETEER_HEADLESS:	false,
-		SLOWDOWN_MS:		2000,	// Added to slow down puppeteer to reduce the change of getting blocked
-		PROXY_URL:			"",		// Include protocol (http://, https://...)
+		SLOWDOWN_MS:		1000, 	// Added to slow down puppeteer to reduce the change of getting blocked
+		PROXY_URL:			"",		// Include protocol (http://, https://)
 		PROXY_AUTH:			{
-			username: "",
-			password: "",
-		},
-		BROWSER_LIFE_SEC:	120,
+								username: "",
+								password: "",
+							},
+		BROWSER_LIFE_SEC:	30,
+		NAV_TIMEOUT_MS:		10000,
 	}
 
 };
