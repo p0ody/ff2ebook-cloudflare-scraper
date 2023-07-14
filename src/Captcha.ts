757430105c45b75b805133d91c5b7507ec0f3bb7
p0ody <mcliche91@gmail.com>
2023-07-14 19:51:47 -0400

    Removed unused function

--- src/Captcha.ts
+++ src/Captcha.ts
@@ -1,38 +1,6 @@
 import Playwright from "playwright";
 import Logger from "loglevel";
 
-/** Return true if it succeeded to solve the captcha and false if it failed */
-export async function handleCaptcha(page: Playwright.Page): Promise<boolean> { // Check if a captcha is present and try to complete it
-    await page.waitForLoadState("domcontentloaded");
-    const content = await page.content().catch((err) => { throw err });
-    if (!content) {
-        return false;
-    }
-
-    if (content.includes("Attention Required! | Cloudflare")) { // Random blocking
-        return false;
-    }
-
-    if (content.includes("hcaptcha-box")
-    ||  content.includes("turnstile-wrapper")) { // Clickable captha
-        Logger.info("trying check captcha");
-        //page.locator(`.ctp-checkbox-label input[type="checkbox"]`).click();
-        //const checkbox = page.locator("css=.ctp-checkbox-label");
-        //await page.mouse.move(65, 280);
-        await page.mouse.click(65, 290);
-        await delay(1000);
-        return true;
-    }
-
-    if (content.includes("big-button")) {
-        const button = page.locator("css=.big-button");
-        Logger.info("trying button captcha");
-        button.click();
-        return true;
-    }
-
-    return false;
-}
 
 export async function solveCaptcha(page: Playwright.Page): Promise<boolean> {
     
